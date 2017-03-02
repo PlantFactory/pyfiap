0880cc8038442ac69a4e8f28a0c6a1db54ff45c0
Makoto Uju <ainehanta@hotmail.co.jp>
2017-03-02 21:45:45 +0900

    suds-jurkoに変更

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 from pyfiap import __author__, __version__, __license__
 
 setup(
-        name             = 'pyfiap',
-        version          = __version__,
-        description      = 'FIAP library for python',
-        license          = __license__,
-        author           = __author__,
-        author_email     = 'none',
-        url              = 'https://github.com/PlantFactory/pyfiap',
-        keywords         = 'python fiap ieee1888',
-        packages         = find_packages(),
-        install_requires = ['suds'],
-        )
+    name             = 'pyfiap',
+    version          = __version__,
+    description      = 'FIAP library for python',
+    license          = __license__,
+    author           = __author__,
+    author_email     = 'none',
+    url              = 'https://github.com/PlantFactory/pyfiap',
+    keywords         = 'python fiap ieee1888',
+    packages         = find_packages(),
+    install_requires = ['suds-jurko'],
+)
